223bd4061416ee49e79656eb8bd6204cb232fe3f
uday kiran <uk144199@gmail.com>
2026-03-03 09:47:40 +0530

    first commit

---
+++ src/test/java/com/abc/demo/JenkinsExample/First.java
@@ -0,0 +1,25 @@
+package com.abc.demo.JenkinsExample;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class First {
+	
+	@Test
+	public void validLogin()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://practicetestautomation.com/practice-test-login/");
+		driver.findElement(By.id("username")).sendKeys("student");
+		driver.findElement(By.id("password")).sendKeys("Password123");
+		driver.findElement(By.id("submit")).click();
+		
+		String actualOutput = driver.findElement(By.className("post-title")).getText();
+		assertEquals(actualOutput, "Logged In Successfully");
+	}
+
+}
