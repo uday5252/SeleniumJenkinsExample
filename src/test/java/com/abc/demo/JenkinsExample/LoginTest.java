c98e2a715459b4498fa21fe2f03ebe124d7302e2
uday kiran <uk144199@gmail.com>
2026-03-03 09:49:38 +0530

    second commit

--- src/test/java/com/abc/demo/JenkinsExample/LoginTest.java
+++ src/test/java/com/abc/demo/JenkinsExample/LoginTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class First {
+public class LoginTest {
 	
 	@Test
 	public void validLogin()
